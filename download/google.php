bb2793facfe94a34b2ce03258ad1c13491b12241
chopin <chopins.xiao@gmail.com>
2014-11-28 18:54:16 +0800

    update

---
+++ download/google.php
@@ -0,0 +1,43 @@
+<?php
+set_time_limit(0);
+$url = 'http://'.$_SERVER['HTTP_HOST']. $_SERVER['REQUEST_URI'];
+
+$dir = pathinfo($url);
+$filename = basename($dir['dirname']);
+$range = 0;
+if(isset($_SERVER['HTTP_RANGE'])){
+    $range = $_SERVER['HTTP_RANGE'];
+}
+
+$da = '';
+if(isset($_COOKIE['MarketDA'])) {
+    $da = $_COOKIE['MarketDA'];
+}
+$opts = array(
+    'http' => array(
+        'method' => "GET",
+        'header' => "Accept-language: en\r\n" .
+        "Cookie: MarketDA={$da}\r\n" .
+        "User-Agent: AndroidDownloadManager/4.4.2 (Linux; U; Android 4.4.2; GT-I9508 Build/KOT49H)\r\n"
+    )
+);
+$context = stream_context_create($opts);
+
+$fp = @fopen('http://proxy.toknot.com/down.php?url='.  urlencode($url).'&da='.urlencode($da).'&range='.  urlencode($range),'r', false, $context);
+
+$stat = stream_get_meta_data($fp);
+
+if (isset($stat['wrapper_data'])) {
+    foreach ($stat['wrapper_data'] as $i => $header) {
+        if ($i == 0) {
+            continue;
+        }
+        header($header);
+    }
+} else {
+    header("Content-type: application/vnd.android.package-delta;charset=utf-8");
+    header("Accept-Ranges:bytes");
+}
+header('Content-Disposition: attachment; filename="' . $filename . '"');
+fpassthru($fp);
+fclose($fp);
