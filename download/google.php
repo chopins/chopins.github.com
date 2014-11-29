5b49896fde769e08a7d6bf7a2942aab2c20b1da5
chopin xiao <chopins.xiao@gmail.com>
2014-11-30 00:39:57 +0800

    update

--- download/google.php
+++ download/google.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set('Etc/GMT-8');
 set_time_limit(0);
 $url = 'http://'.$_SERVER['HTTP_HOST']. $_SERVER['REQUEST_URI'];
 
@@ -9,17 +9,26 @@ $range = 0;
 if(isset($_SERVER['HTTP_RANGE'])){
     $range = $_SERVER['HTTP_RANGE'];
 }
-print_r(getallheaders());
-$da = '';
-if(isset($_COOKIE['MarketDA'])) {
-    $da = $_COOKIE['MarketDA'];
+
+
+$da = $connect = $cookie = '';
+$header = '';
+foreach($_SERVER as  $field => $fv) {
+    if(strpos($field, 'HTTP_') === 0) {
+        $filename = str_replace(' ','-',ucwords(strtolower(str_replace(array('HTTP_','_'), array('',' '), $field))));
+        $header .= "$filename: $fv\r\n";
+    }
 }
+$body = '';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $body = file_get_contents('php://input');
+}
+
 $opts = array(
     'http' => array(
         'method' => "GET",
-        'header' => "Accept-language: en\r\n" .
-        "Cookie: MarketDA={$da}\r\n" .
-        "User-Agent: AndroidDownloadManager/4.4.2 (Linux; U; Android 4.4.2; GT-I9508 Build/KOT49H)\r\n"
+        'header' => $header,
+        'content' => $body
     )
 );
 $context = stream_context_create($opts);
@@ -44,6 +53,6 @@ if (isset($stat['wrapper_data'])) {
     @header("Content-type: application/vnd.android.package-delta;charset=utf-8");
     @header("Accept-Ranges:bytes");
 }
-@header('Content-Disposition: attachment; filename="' . $filename . '.apk"');
+//@header('Content-Disposition: attachment; filename="' . $filename . '.apk"');
 @fpassthru($fp);
 @fclose($fp);
