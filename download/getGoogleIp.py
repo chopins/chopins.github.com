b8e04310a713ecc3d26cc94d9d657d4b6a0a27a5
chopin xiao <chopins.xiao@gmail.com>
2014-12-28 19:00:30 +0800

    update

--- download/getGoogleIp.py
+++ download/getGoogleIp.py
@@ -8,15 +8,37 @@ import pprint
 import sys
 import time
 import signal
-from multiprocessing.dummy import Pool as ThreadPool 
+from multiprocessing.dummy import Pool as ThreadPool
+import math
 
-cur = os.getcwd()
-storedir = "%s/dnsdatapy/" % cur
-useOpenSSL = False
+class GlobalValue:
+    cur = os.getcwd()
+    storedir = "%s/dnsdatapy/" % cur
+    useOpenSSL = False
+    check_num = 0
+    count_ip_num = 0
+    complete = 0
+    mutex = threading.Lock()
+    fs_ip = None
+    fs_g = None
+    fs_gs = None
+    fs_gu = None
+    fs_yb = None
+    fs_gg = None
+    fs_cg = None
+    fs_ga = None
+    fs_ap = None
+    fs_gv = None
+    fs_yg = None
+    fs_ban = None
+    fs_banip_list = []
+    def __init__(self):
+        pass
+
 if sys.version_info.major == 2 and sys.version_info.micro < 9:
     try:
         import OpenSSL
-        useOpenSSL = True
+        GlobalValue.useOpenSSL = True
         if OpenSSL.__version__.split('.')[1] < 12:
             print('Your pyOpenSSL version less than 0.12, update to higher version')
             sys.exit()
@@ -65,14 +87,13 @@ def getnetblocks():
     return block
 
 def checkstroedir():
-    global storedir
-    if(os.path.isdir(storedir)):
+    if(os.path.isdir(GlobalValue.storedir)):
         pass
     else:
         os.mkdir(storedir)
 
 def write_ip_txt():
-    f = "%siplist.txt" % storedir
+    f = "%siplist.txt" % GlobalValue.storedir
     fs = open(f, 'w')
     return fs
 
@@ -103,32 +124,35 @@ ns4 IN A 216.239.38.10
 ''' % date
 
 def writer_default(domain):
-    f = "%s%s.zone" % (storedir,domain)
+    f = "%s%s.zone" % (GlobalValue.storedir,domain)
     fs = open(f, 'w')
     fs.write(zone_default_conf())
     fs.flush()
     return fs
 
 def write_google_default():
-    f = "%sgoogle.com.zone" % storedir
+    f = "%sgoogle.com.zone" % GlobalValue.storedir
     fs = open(f, 'w')
     fs.write(google_ns_ip())
     fs.flush()
     return fs
 
 def init_write_zone():
-    global ipfs,gfs,gsfs,gufs,ybfs,ggfs,cgfs,gafs,apfs,gvfs,ygfs
-    ipfs = write_ip_txt()
-    gfs = write_google_default()
-    gsfs = writer_default('gstatic.com')
-    gufs = writer_default('googleusercontent.com')
-    ybfs = writer_default('youtube.com')
-    ggfs = writer_default('ggpht.com')
-    cgfs = writer_default('clients.google.com')    
-    gafs = writer_default('googleapis.com')
-    apfs = writer_default('appspot.com')
-    gvfs = writer_default('googlevideo.com')
-    ygfs = writer_default('ytimg.com')
+    GlobalValue.fs_ip = write_ip_txt()
+    GlobalValue.fs_g = write_google_default()
+    GlobalValue.fs_gs = writer_default('gstatic.com')
+    GlobalValue.fs_gu = writer_default('googleusercontent.com')
+    GlobalValue.fs_yb = writer_default('youtube.com')
+    GlobalValue.fs_gg = writer_default('ggpht.com')
+    GlobalValue.fs_cg = writer_default('clients.google.com')    
+    GlobalValue.fs_ga = writer_default('googleapis.com')
+    GlobalValue.fs_ap = writer_default('appspot.com')
+    GlobalValue.fs_gv = writer_default('googlevideo.com')
+    GlobalValue.fs_yg = writer_default('ytimg.com')
+    f = "%sip_ban.list" % GlobalValue.storedir
+    GlobalValue.fs_ban = open(f,'a+')
+    for line in GlobalValue.fs_ban:
+        GlobalValue.fs_banip_list.append(line.strip())
 
 def ip2long(ipstr):
     return struct.unpack("!I", socket.inet_aton(ipstr))[0]
@@ -277,96 +301,111 @@ class GenerateRecord:
             self.check_ip_domain(fp, check_domain, domain, key)
 
 def checkCert(hostip):
-    global useOpenSSL,ipfs,gfs,gsfs,gufs,ybfs,ggfs,cgfs,gafs,apfs,gvfs,ygfs
-    socket.setdefaulttimeout(3)
-    if useOpenSSL:
-        cert_info = openssl_check(hostip)
-    else:
-        cert_info = sslconnect(hostip)
-        
-    if not cert_info:
-        return
-    domain_str = str(cert_info[1])
-    ipfs.write("IP %s Valid Domain: %s\n" % (hostip, domain_str))
-    ipfs.flush()
-    gr = GenerateRecord(cert_info, hostip)
-    
-    gcomm_sn = '8924155373108256736'
-    
-    gr.writer_a_reocrd(gfs, 'google.com', '@',gcomm_sn)
-    gr.check_ip_domain(gfs, 'www.google.com', None,'3560456681580786076')
-    gmsn = '7810596706032786260'
-    dmlist = ('mail.google.com','inbox.google.com')
-    for d in dmlist:
-        gr.check_ip_domain(gfs,d, None, gmsn)
-    
-    gr.check_ip_domain(gfs, 'accounts.google.com', None, '3059601525857341613')
-    gr.check_ip_domain(gfs, 'm.google.com', None, '7422046862831836265')
-    gr.check_ip_domain(gfs, 'checkout.google.com', None, '2412261313296579961')
-    comm_domain_list = ('talk.google.com','plus.google.com','play.google.com',
-                        'id.google.com','groups.google.com','images.google.com',
-                        'code.google.com','map.google.com','maps.google.com',
-                        'news.google.com','upload.google.com','drive.google.com',
-                        'encrypted.google.com','translate.google.com')
-    for d in comm_domain_list:
-        gr.check_ip_domain(gfs, d, '*.google.com', gcomm_sn)
+    try:
+        GlobalValue.mutex.acquire()
+        GlobalValue.check_num = GlobalValue.check_num + 1
+        GlobalValue.mutex.release()
+        if(GlobalValue.count_ip_num > 0):
+            curr = math.floor(float(GlobalValue.check_num) / float(GlobalValue.count_ip_num) * 100)
+            if curr > GlobalValue.complete:
+                GlobalValue.complete = curr
+                print('Complete:%d%%' % GlobalValue.complete)
 
-    gr.check_ip_list(gfs,'encrypted-tbn', 0, 3, '.google.com', '*.google.com',gcomm_sn)
-    gr.check_ip_list(gfs,'drive', 0, 9, '.google.com', '*.google.com',gcomm_sn)
-    gr.writer_a_reocrd(gfs, '*.google.com', '*', gcomm_sn)
-    gfs.flush()
-    gr.writer_a_reocrd(cgfs, '*.clients.google.com', '*')
-    
-    gssn = '4629298540228774186'
-    gr.writer_a_reocrd(gsfs, 'gstatic.com', '@', gssn)
-    gs_domain_list = ('fonts.gstatic.com','csi.gstatic.com','maps.gstatic.com','www.gstatic.com','ssl.gstatic.com')
-    for gd in gs_domain_list:
-        gr.check_ip_domain(gsfs, gd, '*.gstatic.com', gssn)
-        
-    gr.check_ip_list(gsfs, 'g', 0, 3, '.gstatic.com', '*.gstatic.com', gssn)
-    gr.check_ip_list(gsfs, 'mt', 0, 7, '.gstatic.com', '*.gstatic.com', gssn)
-    gr.check_ip_list(gsfs, 't', 0, 3, '.gstatic.com', '*.gstatic.com', gssn)
-    gr.check_ip_list(gsfs, 'encrypted-tbn', 0, 3, '.gstatic.com', '*.gstatic.com', gssn)
-    gr.writer_a_reocrd(gsfs, '*.gstatic.com', '*', gssn)
-    gsfs.flush()
-    gr.writer_a_reocrd(gufs, 'googleusercontent.com')
-    gr.writer_a_reocrd(gufs, '*.googleusercontent.com', '*', '1423458468341525840')
-    gufs.flush()
-    gr.writer_a_reocrd(ybfs, 'youtube.com')
-    ybsn = '8924155373108256736'
-    yb_domain_list = ('www.youtube.com','accounts.youtube.com','help.youtube.com','m.youtube.com','insight.youtube.com')
-    for ybd in yb_domain_list:
-        gr.check_ip_domain(ybfs, ybd, '*.youtube.comm', ybsn)
-    gr.writer_a_reocrd(ybfs, '*.youtube.com', '*', ybsn)
-    ybfs.flush()
-    ggsn = '1423458468341525840'
-    gr.writer_a_reocrd(ggfs, 'ggpht.com', '@')
-    gr.check_ip_list(ggfs, 'lh', 3, 6, '.ggpht.com', '*.ggpht.com', ggsn)
-    gr.check_ip_list(ggfs, 'gm', 1, 4, '.ggpht.com', '*.ggpht.com', ggsn)
-    gr.check_ip_list(ggfs, 'geo', 1, 3, '.ggpht.com', '*.ggpht.com', ggsn)
-    gr.writer_a_reocrd(ggfs, '*.ggpht.com', '*', gssn)
-    ggfs.flush()
-    gapi_sn = '9025913279998864902'
-    gapi_domain = ('ajax.googleapis.com','fonts.googleapis.com','chart.googleapis.com',
-                    'maps.googleapis.com','wwww.googleapis.com','play.googleapis.com',
-                    'translate.googleapis.com','youtube.googleapis.com','content.googleapis.com',
-                    'bigcache.googleapis.com','storage.googleapis.com','android.googleapis.com',
-                    'redirector-bigcache.googleapis.com','commondatastorage.googleapis.com','bigcache.googleapis.com')
-    for gapid in gapi_domain:
-        gr.check_ip_domain(gafs, gapid, '*.googleapis.com', gapi_sn)
-    
-    gr.check_ip_list(gafs, 'mt', 0, 3, '.googleapis.com', '*.googleapis.com', gapi_sn)
-    gafs.flush()
-    gr.writer_a_reocrd(gafs, '*.googleapis.com')
-    gr.writer_a_reocrd(apfs, '*.appspot.com')
-    gr.writer_a_reocrd(gvfs, '*.googlevideo.com')
-    gvfs.flush()
-    gr.writer_a_reocrd(ygfs, 'ytimg.com')
-    gr.check_ip_domain(ygfs, 'i.ytimg.com', '*.ytimg.com', );
-    gr.check_ip_list(ygfs, 'i', 1, 4, '.ytimg.com', '*.ytimg.com', gcomm_sn);
-    gr.check_ip_domain(ygfs, 's.ytimg.com', '*.ytimg.com', gcomm_sn);
-    gr.writer_a_reocrd(ygfs, '*.ytimg.com', '*', gcomm_sn);
-    ygfs.flush()
+        socket.setdefaulttimeout(3)
+        if GlobalValue.useOpenSSL:
+            cert_info = openssl_check(hostip)
+        else:
+            cert_info = sslconnect(hostip)
+
+        if not cert_info:
+            GlobalValue.fs_ban.write("%s\n" % hostip)
+            GlobalValue.fs_ban.flush()
+            return
+
+        domain_str = str(cert_info[1])
+
+        GlobalValue.fs_ip.write("IP %s Valid Domain: %s\n" % (hostip, domain_str))
+        GlobalValue.fs_ip.flush()
+        gr = GenerateRecord(cert_info, hostip)
+
+        gcomm_sn = '8924155373108256736'
+
+        gr.writer_a_reocrd(GlobalValue.fs_g, 'google.com', '@',gcomm_sn)
+        gr.check_ip_domain(GlobalValue.fs_g, 'www.google.com', None,'3560456681580786076')
+        gmsn = '7810596706032786260'
+        dmlist = ('mail.google.com','inbox.google.com')
+        for d in dmlist:
+            gr.check_ip_domain(GlobalValue.fs_g,d, None, gmsn)
+
+        gr.check_ip_domain(GlobalValue.fs_g, 'accounts.google.com', None, '3059601525857341613')
+        gr.check_ip_domain(GlobalValue.fs_g, 'm.google.com', None, '7422046862831836265')
+        gr.check_ip_domain(GlobalValue.fs_g, 'checkout.google.com', None, '2412261313296579961')
+        comm_domain_list = ('talk.google.com','plus.google.com','play.google.com',
+                            'id.google.com','groups.google.com','images.google.com',
+                            'code.google.com','map.google.com','maps.google.com',
+                            'news.google.com','upload.google.com','drive.google.com',
+                            'encrypted.google.com','translate.google.com')
+        for d in comm_domain_list:
+            gr.check_ip_domain(GlobalValue.fs_g, d, '*.google.com', gcomm_sn)
+
+        gr.check_ip_list(GlobalValue.fs_g,'encrypted-tbn', 0, 3, '.google.com', '*.google.com',gcomm_sn)
+        gr.check_ip_list(GlobalValue.fs_g,'drive', 0, 9, '.google.com', '*.google.com',gcomm_sn)
+        gr.writer_a_reocrd(GlobalValue.fs_g, '*.google.com', '*', gcomm_sn)
+        gfs.flush()
+        gr.writer_a_reocrd(GlobalValue.fs_cg, '*.clients.google.com', '*')
+
+        gssn = '4629298540228774186'
+        gr.writer_a_reocrd(GlobalValue.fs_gs, 'gstatic.com', '@', gssn)
+        gs_domain_list = ('fonts.gstatic.com','csi.gstatic.com','maps.gstatic.com','www.gstatic.com','ssl.gstatic.com')
+        for gd in gs_domain_list:
+            gr.check_ip_domain(GlobalValue.fs_gs, gd, '*.gstatic.com', gssn)
+
+        gr.check_ip_list(GlobalValue.fs_gs, 'g', 0, 3, '.gstatic.com', '*.gstatic.com', gssn)
+        gr.check_ip_list(GlobalValue.fs_gs, 'mt', 0, 7, '.gstatic.com', '*.gstatic.com', gssn)
+        gr.check_ip_list(GlobalValue.fs_gs, 't', 0, 3, '.gstatic.com', '*.gstatic.com', gssn)
+        gr.check_ip_list(GlobalValue.fs_gs, 'encrypted-tbn', 0, 3, '.gstatic.com', '*.gstatic.com', gssn)
+        gr.writer_a_reocrd(GlobalValue.fs_gs, '*.gstatic.com', '*', gssn)
+        gsfs.flush()
+        gr.writer_a_reocrd(GlobalValue.fs_gu, 'googleusercontent.com')
+        gr.writer_a_reocrd(GlobalValue.fs_gu, '*.googleusercontent.com', '*', '1423458468341525840')
+        gufs.flush()
+        gr.writer_a_reocrd(GlobalValue.fs_yb, 'youtube.com')
+        ybsn = '8924155373108256736'
+        yb_domain_list = ('www.youtube.com','accounts.youtube.com','help.youtube.com','m.youtube.com','insight.youtube.com')
+        for ybd in yb_domain_list:
+            gr.check_ip_domain(GlobalValue.fs_yb, ybd, '*.youtube.comm', ybsn)
+        gr.writer_a_reocrd(GlobalValue.fs_yb, '*.youtube.com', '*', ybsn)
+        ybfs.flush()
+        ggsn = '1423458468341525840'
+        gr.writer_a_reocrd(GlobalValue.fs_gg, 'ggpht.com', '@')
+        gr.check_ip_list(GlobalValue.fs_gg, 'lh', 3, 6, '.ggpht.com', '*.ggpht.com', ggsn)
+        gr.check_ip_list(GlobalValue.fs_gg, 'gm', 1, 4, '.ggpht.com', '*.ggpht.com', ggsn)
+        gr.check_ip_list(GlobalValue.fs_gg, 'geo', 1, 3, '.ggpht.com', '*.ggpht.com', ggsn)
+        gr.writer_a_reocrd(GlobalValue.fs_gg, '*.ggpht.com', '*', gssn)
+        ggfs.flush()
+        gapi_sn = '9025913279998864902'
+        gapi_domain = ('ajax.googleapis.com','fonts.googleapis.com','chart.googleapis.com',
+                        'maps.googleapis.com','wwww.googleapis.com','play.googleapis.com',
+                        'translate.googleapis.com','youtube.googleapis.com','content.googleapis.com',
+                        'bigcache.googleapis.com','storage.googleapis.com','android.googleapis.com',
+                        'redirector-bigcache.googleapis.com','commondatastorage.googleapis.com','bigcache.googleapis.com')
+        for gapid in gapi_domain:
+            gr.check_ip_domain(GlobalValue.fs_ga, gapid, '*.googleapis.com', gapi_sn)
+
+        gr.check_ip_list(GlobalValue.fs_ga, 'mt', 0, 3, '.googleapis.com', '*.googleapis.com', gapi_sn)
+        gafs.flush()
+        gr.writer_a_reocrd(GlobalValue.fs_ga, '*.googleapis.com')
+        gr.writer_a_reocrd(GlobalValue.fs_ap, '*.appspot.com')
+        gr.writer_a_reocrd(GlobalValue.fs_gv, '*.googlevideo.com')
+        gvfs.flush()
+        gr.writer_a_reocrd(GlobalValue.fs_yg, 'ytimg.com')
+        gr.check_ip_domain(GlobalValue.fs_yg, 'i.ytimg.com', '*.ytimg.com', );
+        gr.check_ip_list(GlobalValue.fs_yg, 'i', 1, 4, '.ytimg.com', '*.ytimg.com', gcomm_sn);
+        gr.check_ip_domain(GlobalValue.fs_yg, 's.ytimg.com', '*.ytimg.com', gcomm_sn);
+        gr.writer_a_reocrd(GlobalValue.fs_yg, '*.ytimg.com', '*', gcomm_sn);
+        ygfs.flush()
+    except:
+        print(sys.exc_info())
 
 def check_organization(subject):
     if useOpenSSL and subject.organizationName == 'Google Inc':
@@ -376,6 +415,11 @@ def check_organization(subject):
             for i in item:
                 if i[0] == 'organizationName' and i[1] == 'Google Inc':
                     return True
+
+def in_ban_ip(ip):
+    if ip in GlobalValue.fs_banip_list:
+        return True
+    return False
 
 class connetThread(threading.Thread):
     def __init__(self,startip,maxip):
@@ -388,53 +432,55 @@ class connetThread(threading.Thread):
     def run(self):
         connip = self.startip
         iplist = []
-        pool = ThreadPool(10)
+        pool = ThreadPool()
         while connip < self.maxip:
             host = long2ip(connip)
-            
-            iplist.append(host)
-            if len(iplist) >=10:
-                pool.map(checkCert,iplist)
-                pool.close()
-                pool.join()
-                pool = ThreadPool(10)
-            
+            if not in_ban_ip(host):
+                iplist.append(host)
+                if len(iplist) >=100:
+                    pool.map(checkCert,iplist)
+                    pool.close()
+                    pool.join()
+                    iplist = []
+                    pool = ThreadPool()
+
             connip = connip + 1
 
 def closefs():
-    global ipfs,gfs,gsfs,gufs,ybfs,ggfs,cgfs,gafs,apfs,gvfs,ygfs
-    ipfs.close()
-    gfs.close()
-    gsfs.close()
-    gufs.close()
-    ybfs.close()
-    ggfs.close()
-    cgfs.close()
-    gafs.close()
-    apfs.close()
-    gvfs.close()
-    ygfs.close()
+    GlobalValue.fs_ip.close()
+    GlobalValue.fs_g.close()
+    GlobalValue.fs_gs.close()
+    GlobalValue.fs_gu.close()
+    GlobalValue.fs_yb.close()
+    GlobalValue.fs_gg.close()
+    GlobalValue.fs_cg.close()
+    GlobalValue.fs_ga.close()
+    GlobalValue.fs_ap.close()
+    GlobalValue.fs_gv.close()
+    GlobalValue.fs_yg.close()
 
 def getGoogleIp():
-    global ipfs,gfs,gsfs,gufs,ybfs,ggfs,cgfs,gafs,apfs,gvfs,ygfs
-   
     blocklist = getnetblocks()
     
     checkstroedir()
     init_write_zone()
     tlen = 0
+    thread_count_ip_num = 0;
     for iptxt in blocklist:
         iptxt = iptxt.lstrip('ip4:')
         (startip,endip,start,maxip) = getIpRange(iptxt)
-        str = "IP BLOCK:{%s} IN { {%s} -- {%s} }" % (iptxt,startip,endip);
-        ipfs.write(str)
-        
+        str = "IP BLOCK:{%s} IN { {%s} -- {%s} }\n" % (iptxt,startip,endip);
+        GlobalValue.fs_ip.write(str)
+        GlobalValue.fs_ip.flush()
+        thread_count_ip_num = thread_count_ip_num + (maxip - start)
         while(tlen >50):
             time.sleep(1)
             tlen = threading.active_count()
 
         connetThread(start,maxip)
 
+    GlobalValue.count_ip_num = thread_count_ip_num
+    print('Count Ip:%s' % GlobalValue.count_ip_num)
     tlen = threading.active_count()
     signal.signal(signal.SIGINT, process_term)  
     while(tlen >0):
