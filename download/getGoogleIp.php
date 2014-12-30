8f21c53f09ded16136dd3c5a3c649b45c52bcafe
chopin <chopins.xiao@gmail.com>
2014-12-30 13:14:39 +0800

    update

--- download/getGoogleIp.php
+++ download/getGoogleIp.php
@@ -2,12 +2,12 @@
 <?php
 date_default_timezone_set('UTC');
 
-if(!extension_loaded('openssl')) {
+if (!extension_loaded('openssl')) {
     exit('need openssl extension to support https' . PHP_EOL);
 }
 
-if(!extension_loaded('pcntl')) {
-    exit('need pcntl extension to support multi-process'. PHP_EOL);
+if (!extension_loaded('pcntl')) {
+    exit('need pcntl extension to support multi-process' . PHP_EOL);
 }
 
 $start_time = time();
@@ -199,7 +199,7 @@ foreach ($blockList as $ipblock) {
                 }
 
                 fwrite($ip_list_fp, "IP $ip Valid Domain: {$d}\n");
-                $ipkey = $cerInfo['extensions']['subjectKeyIdentifier'];
+                $ipsn = $cerInfo['serialNumber'];
                 fclose($r);
 
                 $dms = explode(',', $d);
@@ -242,89 +242,88 @@ function check_cert_info() {
     extract($GLOBALS);
 
     $all_skip = false;
-    $gc_check_key = '07:9A:CE:FB:13:97:D9:C7:E2:8E:DD:3B:F2:16:36:B5:9A:EF:B6:99';
-    $www_key = '9E:9F:AE:7B:6E:20:8A:68:22:ED:81:36:DA:97:5E:50:2A:73:A3:C3';
-    $gm_check_key = '4A:38:1F:6A:25:E2:0A:C9:4A:EC:05:0E:17:3C:32:FA:89:56:28:B6';
-    $account_key = '58:1E:61:FF:39:01:FD:E1:43:02:05:6C:84:95:2F:2A:10:32:BD:4C';
-    $checkout_key = '42:2B:E3:5B:05:9F:74:E6:E5:EB:84:B9:0A:34:9D:C6:79:08:2C:F9';
-    $google_m_key = '27:42:2D:12:83:34:C4:88:EE:68:1C:71:FF:E7:31:B7:5E:CA:4B:F0';
-    $gusercontent_key = '49:EC:70:2C:FE:BB:8E:0F:17:18:45:4B:26:2D:F4:01:84:C7:90:44';
-    $gst_check_key = 'A5:F4:B3:DD:B9:29:9C:2E:50:A1:2A:17:45:3C:9C:70:01:EF:56:2A';
+    $gc_sn = '8924155373108256736';
+    $www_sn = '3560456681580786076';
+    $gm_sn = '7810596706032786260';
+    $account_sn = '3059601525857341613';
+    $checkout_sn = '2412261313296579961';
+    $google_m_sn = '7422046862831836265';
+    $guc_sn = '1423458468341525840';
+    $gst_sn = '4629298540228774186';
+    $gapi_sn = '9025913279998864902';
+    writer_a_rec($upgoogle, 'google.com', '@', $gc_sn);
 
-    writer_a_rec($upgoogle, 'google.com', '@', $gc_check_key);
-
-    check_ip_domain($upgoogle, 'www.google.com', null, $www_key);
+    check_ip_domain($upgoogle, 'www.google.com', null, $www_sn);
     $mail_list = array('mail', 'inbox');
     foreach ($mail_list as $mail_pre) {
-        check_ip_domain($upgoogle, "{$mail_pre}.google.com", null, $gm_check_key);
+        check_ip_domain($upgoogle, "{$mail_pre}.google.com", null, $gm_sn);
     }
 
-    check_ip_domain($upgoogle, 'accounts.google.com', null, $account_key);
-    check_ip_domain($upgoogle, 'm.google.com', null, $google_m_key);
-    check_ip_domain($upgoogle, 'checkout.google.com', null, $checkout_key);
+    check_ip_domain($upgoogle, 'accounts.google.com', null, $account_sn);
+    check_ip_domain($upgoogle, 'm.google.com', null, $google_m_sn);
+    check_ip_domain($upgoogle, 'checkout.google.com', null, $checkout_sn);
 
     $gc_list = array('talk', 'plus', 'play', 'id', 'groups', 'images', 'code', 'map', 'maps', 'news', 'upload', 'dirve', 'encrypted', 'translate', 'clients2');
     foreach ($gc_list as $domain_pre) {
-        check_ip_domain($upgoogle, "{$domain_pre}.google.com", '*.google.com', $gc_check_key);
+        check_ip_domain($upgoogle, "{$domain_pre}.google.com", '*.google.com', $gc_sn);
     }
 
-    check_ip_list($upgoogle, 'encrypted-tbn', 0, 3, '.google.com', '*.google.com', $gc_check_key);
+    check_ip_list($upgoogle, 'encrypted-tbn', 0, 3, '.google.com', '*.google.com', $gc_sn);
 
-    writer_a_rec($upgoogle, '*.google.com', '*', $gc_check_key);
+    writer_a_rec($upgoogle, '*.google.com', '*', $gc_sn);
 
     writer_a_rec($clientsgoogle, '*.clients.google.com', '*');
 
-    writer_a_rec($gstatic, 'gstatic.com', '@', $gst_check_key);
+    writer_a_rec($gstatic, 'gstatic.com', '@', $gst_sn);
     $gstatc_list = array('ssl', 'fonts', 'csi', 'maps', 'www');
     foreach ($gstatc_list as $gstatic_pre) {
-        check_ip_domain($gstatic, "{$gstatic_pre}.gstatic.com", '*.gstatic.com', $gst_check_key);
+        check_ip_domain($gstatic, "{$gstatic_pre}.gstatic.com", '*.gstatic.com', $gst_sn);
     }
 
-    check_ip_list($gstatic, 'g', 0, 3, '.gstatic.com', '*.gstatic.com', $gst_check_key);
-    check_ip_list($gstatic, 'mt', 0, 7, '.gstatic.com', '*.gstatic.com', $gst_check_key);
-    check_ip_list($gstatic, 't', 0, 3, '.gstatic.com', '*.gstatic.com', $gst_check_key);
-    check_ip_list($gstatic, 'encrypted-tbn', 0, 3, '.gstatic.com', '*.gstatic.com', $gst_check_key);
-    writer_a_rec($gstatic, '*.gstatic.com', '*', $gst_check_key);
+    check_ip_list($gstatic, 'g', 0, 3, '.gstatic.com', '*.gstatic.com', $gst_sn);
+    check_ip_list($gstatic, 'mt', 0, 7, '.gstatic.com', '*.gstatic.com', $gst_sn);
+    check_ip_list($gstatic, 't', 0, 3, '.gstatic.com', '*.gstatic.com', $gst_sn);
+    check_ip_list($gstatic, 'encrypted-tbn', 0, 3, '.gstatic.com', '*.gstatic.com', $gst_sn);
+    writer_a_rec($gstatic, '*.gstatic.com', '*', $gst_sn);
 
     writer_a_rec($googleusercontent, 'googleusercontent.com', '@');
-    writer_a_rec($googleusercontent, '*.googleusercontent.com', '*', $gusercontent_key);
+    writer_a_rec($googleusercontent, '*.googleusercontent.com', '*', $guc_sn);
 
     writer_a_rec($youtube, 'youtube.com');
     $youtube_list = array('www', 'accounts', 'help', 'm', 'insight');
     foreach ($youtube_list as $youtube_pre) {
-        check_ip_domain($youtube, "{$youtube_pre}.youtube.com", '*.youtube.comm', $gc_check_key);
+        check_ip_domain($youtube, "{$youtube_pre}.youtube.com", '*.youtube.comm', $gc_sn);
     }
 
-    writer_a_rec($youtube, '*.youtube.com', '*', $gc_check_key);
+    writer_a_rec($youtube, '*.youtube.com', '*', $gc_sn);
 
     writer_a_rec($ggpht, 'ggpht.com', '@');
-    $ggpht_check_key = '49:EC:70:2C:FE:BB:8E:0F:17:18:45:4B:26:2D:F4:01:84:C7:90:44';
-    check_ip_list($ggpht, 'lh', 3, 6, '.ggpht.com', '*.ggpht.com', $ggpht_check_key);
-    check_ip_list($ggpht, 'gm', 1, 4, '.ggpht.com', '*.ggpht.com', $ggpht_check_key);
-    check_ip_list($ggpht, 'geo', 1, 3, '.ggpht.com', '*.ggpht.com', $ggpht_check_key);
-    writer_a_rec($ggpht, '*.ggpht.com', '*', $ggpht_check_key);
+    check_ip_list($ggpht, 'lh', 3, 6, '.ggpht.com', '*.ggpht.com', $guc_sn);
+    check_ip_list($ggpht, 'gm', 1, 4, '.ggpht.com', '*.ggpht.com', $guc_sn);
+    check_ip_list($ggpht, 'geo', 1, 3, '.ggpht.com', '*.ggpht.com', $guc_sn);
+    writer_a_rec($ggpht, '*.ggpht.com', '*', $guc_sn);
 
-    $gapi_check_key = 'C5:88:7C:4C:4D:AC:7F:AA:48:B5:D6:2B:AA:34:DD:97:B4:2A:3B:5E';
     $api_list = array('ajax', 'fonts', 'chart', 'maps', 'www', 'play', 'translate', 'youtube', 'content', 'bigcache', 'storage', 'android', 'redirector-bigcache', 'commondatastorage');
+    $api_suffix = '.googleapis.com';
     foreach ($api_list as $api_pre) {
-        check_ip_domain($googleapis, "{$api_pre}.googleapis.com", '*.googleapis.com', $gapi_check_key);
+        check_ip_domain($googleapis, "{$api_pre}{$api_suffix}", "*{$api_suffix}", $gapi_sn);
     }
 
-    check_ip_list($googleapis, 'mt', 0, 3, '.googleapis.com', '*.googleapis.com', $gst_check_key);
-    check_ip_list($googleapis, 'mt', 0, 3, '.googleapis.com', '*.googleapis.com', $gst_check_key);
-    check_ip_list($googleapis, 'mt', 0, 3, '.googleapis.com', '*.googleapis.com', $gst_check_key);
-    writer_a_rec($googleapis, '*.googleapis.com', '*', $gapi_check_key);
+    check_ip_list($googleapis, 'mt', 0, 3, '.googleapis.com', '*.googleapis.com', $gst_sn);
+    check_ip_list($googleapis, 'mt', 0, 3, '.googleapis.com', '*.googleapis.com', $gst_sn);
+    check_ip_list($googleapis, 'mt', 0, 3, '.googleapis.com', '*.googleapis.com', $gst_sn);
+    writer_a_rec($googleapis, '*.googleapis.com', '*', $gapi_sn);
 
     writer_a_rec($appspot, '*.appspot.com');
 
-    writer_a_rec($googlevideo, '*.googlevideo.com', '*', $gc_check_key);
+    writer_a_rec($googlevideo, '*.googlevideo.com', '*', $gc_sn);
 
     writer_a_rec($ytimg, 'ytimg.com', '@');
-    check_ip_domain($ytimg, 'i.ytimg.com', '*.ytimg.com', $gc_check_key);
-    check_ip_list($ytimg, 'i', 1, 4, '.ytimg.com', '*.ytimg.com', $gc_check_key);
-    check_ip_domain($ytimg, 's.ytimg.com', '*.ytimg.com', $gc_check_key);
+    check_ip_domain($ytimg, 'i.ytimg.com', '*.ytimg.com', $gc_sn);
+    check_ip_list($ytimg, 'i', 1, 4, '.ytimg.com', '*.ytimg.com', $gc_sn);
+    check_ip_domain($ytimg, 's.ytimg.com', '*.ytimg.com', $gc_sn);
 
-    writer_a_rec($ytimg, '*.ytimg.com', '*', $gc_check_key);
+    writer_a_rec($ytimg, '*.ytimg.com', '*', $gc_sn);
 }
 
 function writer_default($domain) {
@@ -411,7 +410,7 @@ function writer_a_rec(&$fp, $domain, $rec = '*', $key = null) {
         return;
     }
 
-    if (in_array($domain, $dms) && $ipkey == $key) {
+    if (in_array($domain, $dms) && $ipsn == $key) {
         fwrite($fp, "$rec IN A $ip\n");
     }
 }
