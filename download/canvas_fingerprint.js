fdcec3696458e1426a45999d1e166e1ceee0abf0
Szopen Xiao <chopins.xiao@gmail.com>
2015-11-28 17:15:44 +0800

    Create canvas_fingerprint.js

---
+++ download/canvas_fingerprint.js
@@ -0,0 +1,22 @@
+function getfingerprint(text,basetext) {
+    try {
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        var txt = text;
+        ctx.textBaseline = "top";
+        ctx.font = "14px 'Arial'";
+        ctx.textBaseline = basetext;
+        ctx.fillStyle = "#f60";
+        ctx.fillRect(125,1,62,20);
+        ctx.fillStyle = "#069";
+        ctx.fillText(txt, 2, 15);
+        ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
+        ctx.fillText(txt, 4, 17);
+        var b64 = canvas.toDataURL().replace("data:image/png;base64,","");
+        var bin = atob(b64);
+        var crc = encodeURIComponent(bin.slice(-16,-12)).replace(/%/g,'');
+        return crc;
+    } catch(e) {
+        return 0;
+    }
+}
