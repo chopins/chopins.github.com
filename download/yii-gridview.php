d9cd4489941ea381835452a6516240b559f258d4
Szopen Xiao <chopins.xiao@gmail.com>
2022-05-09 20:35:56 +0800

    Update yii-gridview.php

--- download/yii-gridview.php
+++ download/yii-gridview.php
@@ -1,6 +1,28 @@
 <?php
 use yii\grid\GridView;
+use yii\data\ActiveDataProvider;
+
 class Test ｛
    public function indexAction()
-   {}
+   {
+
+     $dataProvider = new ActiveDataProvider([
+        'query' => supplier::find(),
+        'pagination' => [
+        'pageSize' => 50,
+        ],
+      ]);
+      return GridView::widget([
+           'dataProvider' => $dataProvider,
+      'columns' => [
+        [
+            'class' => 'yii\grid\CheckboxColumn',
+            'id',
+            'name',
+            'code',
+            'status',
+        ],
+]);
+
+   }
 ｝
