b446f27fe86c20727f8be4c11a6619d8a94e0b7f
Szopen Xiao <chopins.xiao@gmail.com>
2022-05-09 22:27:33 +0800

    Update yii-gridview.php

--- download/yii-gridview.php
+++ download/yii-gridview.php
@@ -1,22 +1,52 @@
 <?php
 use yii\grid\GridView;
+use yii\base\Model
 use yii\data\ActiveDataProvider;
+class supplier extends Model {
+     public function tableName(){
+     return 'supplier';
+     }
+}
+class FilterModel extends supplier{
+    public function filter($param) {
+         $query = supplier::find();
+         $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+         //数据检查规则略
+         $this->load($param);
+        
+         //以下逻辑应当加上判断是否有下列参数
+         switch($this->idFilter){
+           case '1':
+            $query->andFilterWhere(['>','id', 10]);
+           break;
+           case '2':
+            $query->andFilterWhere(['>=','id', 10]);
+            break;
+            case '3':
+            $query->andFilterWhere(['<','id', 10]);
+            break;
 
-class filterModel {
+            case '4':
+            $query->andFilterWhere(['<=','id', 10]);
+            break;
 
+         }
+         
+         $query->andFilterWhere(['t_status' => $this->t_status]);
+        $query->andFilterWhere(['like', 'name', $this->name]);
+              ->andFilterWhere(['like', 'code', $this->code]);
+        return $dataProvider;
+    }
 }
 class Test ｛
    public function indexAction()
    {
-
-     $dataProvider = new ActiveDataProvider([
-        'query' => supplier::find(),
-        'pagination' => [
-        'pageSize' => 50,
-        ],
-      ]);
+      $filterModel = new FilterModel;
+      $dataProvider = $filterModel->filter(Yii::$app->request->get());
       return GridView::widget([
-           'filterModel' =>
+           'filterModel' => $filterModel,
            'dataProvider' => $dataProvider,
       'columns' => [
         [
@@ -26,12 +56,14 @@ class Test ｛
                     '2'=>'>=10', 
                     '3'=>'<10',
                     '4'=>'<=10',
-                     ],],
+                     ],
+              'filterInputOptions'=>['name'=>'idFilter','id'=>'idFilter']
+            ],
            ['attribute'=> 'name', 
            'filter'=> ''],
            ['attribute'=> 'code',
            'filter'=> ''],
-            ['attribute'=>'status',
+            ['attribute'=>'t_status',
             'filter' =>['ok','hold'],
             ]
         ],
