ec7f856d2eb19ff0112a57d1fe1d8da339ddaa9f
Szopen Xiao <chopins.xiao@gmail.com>
2024-05-15 10:05:37 +0800

    Update any-copy.js

--- download/any-copy.js
+++ download/any-copy.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        http*://*/*
+// @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
@@ -17,14 +18,20 @@
  */
 (function() {
     'use strict';
- let contents = null;
-document.addEventListener("mouseup", (event) => {
-    if(contents.length > 0) {
+ let contents = '';
+document.addEventListener("mousedown", (event) => {
+    if(event.button != 2) {
+        return;
+    }
+    if(contents.length > 0 && window.navigator.clipboard) {
         window.navigator.clipboard.writeText(contents).then(()=>{});
     }
 });
 document.addEventListener("selectionchange", (event) => {
-    contents = document.getSelection().getRangeAt(0).cloneContents().textContent;
+    var sel = document.getSelection();
+    if(sel.rangeCount > 0) {
+     contents = sel.getRangeAt(0).cloneContents().textContent;
+    }
 });
 
 })();
