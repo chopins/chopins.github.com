2f84eebea4fd4e3c940a202b4e94e565abcb26bd
Chopins Xiao <chopins.xiao@gmail.com>
2024-05-30 13:41:01 +0800

    update

---
+++ download/2fa.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         2FA.OTP.TOTP
+// @namespace    https://toknot.com/
+// @version      2024-05-30
+// @author       chopin xiao
+// @updateURL    https://toknot.com/download/2fa.meta.js
+// @@downloadURL https://toknot.com/download/2fa.js
+// @copyright   Toknot.com
+// ==/UserScript==
